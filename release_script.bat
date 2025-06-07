777bc5949b815f1be541e2b92c9605d130a009f7
Miles Eventide <dalton.sterritt@gmail.com>
2025-06-07 05:49:25 -0600

    Updated release scripts

--- release_script.bat
+++ release_script.bat
@@ -4,31 +4,31 @@ REM Create releases directory if it doesn't exist
 mkdir releases
 
 REM Remove existing zip file if it exists
-if exist releases\eventide-teleporter.zip (
-    del releases\eventide-teleporter.zip
+if exist releases\eventide-speech-mannerisms.zip (
+    del releases\eventide-speech-mannerisms.zip
 )
 
 REM Compile files excluding src, node_modules, package.json, and .ignore
 REM Assuming compilation involves copying files to a temp directory
-set TEMP_DIR=%TEMP%\eventide_teleporter_temp
+set TEMP_DIR=%TEMP%\eventide_speech_mannerisms_temp
 mkdir "%TEMP_DIR%"
 
 REM Create a new directory for the release
-mkdir "%TEMP_DIR%\eventide-teleporter"
+mkdir "%TEMP_DIR%\eventide-speech-mannerisms"
 
 REM Copy all files into the new directory
-xcopy * "%TEMP_DIR%\eventide-teleporter\" /E /I /EXCLUDE:exclude.txt
+xcopy * "%TEMP_DIR%\eventide-speech-mannerisms\" /E /I /EXCLUDE:exclude.txt
 
 REM Minify JavaScript files
-node minify.js "%TEMP_DIR%\eventide-teleporter"
+node minify.js "%TEMP_DIR%\eventide-speech-mannerisms"
 
 REM Remove existing zip file if it exists
-if exist releases\eventide-teleporter.zip (
-    del releases\eventide-teleporter.zip
+if exist releases\eventide-speech-mannerisms.zip (
+    del releases\eventide-speech-mannerisms.zip
 )
 
 REM Create the zip file in the releases folder
-powershell -command "Compress-Archive -Path '%TEMP_DIR%\eventide-teleporter' -DestinationPath 'releases\eventide-teleporter.zip'"
+powershell -command "Compress-Archive -Path '%TEMP_DIR%\eventide-speech-mannerisms' -DestinationPath 'releases\eventide-speech-mannerisms.zip'"
 
 REM Clean up the temporary directory
 rmdir /S /Q "%TEMP_DIR%"
--- release_script.sh
+++ release_script.sh
@@ -4,8 +4,8 @@
 mkdir -p releases
 
 # Remove existing zip file if it exists
-if [ -f releases/eventide-teleporter.zip ]; then
-    rm releases/eventide-teleporter.zip
+if [ -f releases/eventide-speech-mannerisms.zip ]; then
+    rm releases/eventide-speech-mannerisms.zip
 fi
 
 # Compile files excluding src, node_modules, package.json, and .ignore
@@ -15,19 +15,19 @@ WORKING_DIR=$(pwd)
 
 
 # Create a new directory for the release
-mkdir -p "$TEMP_DIR/eventide-teleporter"
+mkdir -p "$TEMP_DIR/eventide-speech-mannerisms"
 
 # Copy all files into the new directory
-rsync -av --exclude='src/' --exclude='node_modules/' --exclude=".git/" --exclude='package.json' --exclude='package-lock.json' --exclude='.gitignore' --exclude='.vscode/' --exclude='exclude.txt' --exclude='release_script.sh' --exclude='release_script.bat' --exclude='minify.js' --exclude='releases/' . "$TEMP_DIR/eventide-teleporter/"
+rsync -av --exclude='src/' --exclude='node_modules/' --exclude=".git/" --exclude='package.json' --exclude='package-lock.json' --exclude='.gitignore' --exclude='.vscode/' --exclude='exclude.txt' --exclude='release_script.sh' --exclude='release_script.bat' --exclude='minify.js' --exclude='releases/' . "$TEMP_DIR/eventide-speech-mannerisms/"
 
 # Minify JavaScript files
-node minify.js "$TEMP_DIR/eventide-teleporter"
+node minify.js "$TEMP_DIR/eventide-speech-mannerisms"
 
 # Create the zip file in the temp directory
-cd "$TEMP_DIR" && zip -r eventide-teleporter.zip *
+cd "$TEMP_DIR" && zip -r eventide-speech-mannerisms.zip *
 
 # Move the zip file to the releases directory
-mv "$TEMP_DIR/eventide-teleporter.zip" "$WORKING_DIR/releases/eventide-teleporter.zip"
+mv "$TEMP_DIR/eventide-speech-mannerisms.zip" "$WORKING_DIR/releases/eventide-speech-mannerisms.zip"
 
 # Clean up the temporary directory
 rm -rf "$TEMP_DIR"
